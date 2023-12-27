8d63123cf10f5b89b86692a4705ab8398e5a1cff
SFK <seankelly9696@outlook.com>
2023-12-27 20:46:49 +0000

    Updated Site

--- scripts.js
+++ scripts.js
@@ -1,12 +1,2 @@
 
-document.addEventListener('DOMContentLoaded', function () {
-    const backgroundText = document.querySelector('.background-text');
-    for (let i = 0; i < 50; i++) {
-        let line = document.createElement('div');
-        line.classList.add('background-line');
-        line.textContent = 'Portfolio Coming Soon '.repeat(20); // Repeat the phrase to fill the line
-        line.style.top = `${i * 2}%`; // Adjust the spacing between lines
-        line.style.animationName = i % 2 === 0 ? 'scroll-right' : 'scroll-left'; // Alternate the animation
-        backgroundText.appendChild(line);
-    }
-});
+
